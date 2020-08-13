17a315ae4fa1421af58b128fe0b52720b45e42a5
DrVonDevious <drvondevious@gmail.com>
2020-08-13 14:17:47 -0500

    moved db config to heroku env

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const options = require("./config.json")
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  user: options.db.user,
-  host: options.db.host,
-  database: options.db.database,
-  password: options.db.password,
-  port: options.db.port,
+  user: process.env['DB_USER'],
+  host: process.env['DB_HOST'],
+  database: process.env['DB_DATABASE'],
+  password: process.env['DB_PASSWORD'],
+  port: process.env['DB_PORT'],
 })
 
 // USERS
